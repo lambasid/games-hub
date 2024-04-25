f5aaab91f76af1906532c072e52acd3580b8db81
Siddharth Lamba <siddharthlamba@Siddharths-Laptop.local>
2024-04-24 22:06:35 -0400

    added dynamic list to the code

---
+++ games-hub/src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Dubai", "Toronto"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
