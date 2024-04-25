4404a46bc8193875c9daa53acf477d80f9a0b012
Siddharth Lamba <siddharthlamba@Siddharths-Laptop.local>
2024-04-24 23:20:32 -0400

    minute changes

--- games-hub/src/components/ListGroup.tsx
+++ games-hub/src/components/ListGroup.tsx
@@ -1,9 +1,16 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Dubai", "Toronto"];
+  let items = ["New York", "San Francisco", "Dubai", "Toronto"];
+  items = [];
 
+  const message = items.length === 0 ? <p>No item found.</p> : null;
+
+  const getMessage = () => {
+    
+  }
   return (
     <>
       <h1>List</h1>
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
